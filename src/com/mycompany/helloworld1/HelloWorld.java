4eb45771cc4efb40edf38d6af155afdb5ba30e43
Mon-PC <hichem.barkat3@gmail.com>
2025-04-22 14:24:00 +0100

    innitial commit

---
+++ src/com/mycompany/helloworld1/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mycompany.helloworld1;
+
+import static jdk.internal.org.jline.utils.Colors.s;
+
+public class HelloWorld{
+    public static void main (String...args){
+        system.out.println("helloworld");
+
+
+    }
+}
